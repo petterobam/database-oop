f36c270d2110c3f9c27a95e75eda3aa7e1f8ff34
petterobam <1460300366@qq.com>
2018-04-27 22:40:37 +0800

    sqlite：丰富扩展配置文件，丰富扩展原生配置类，添加第二期封装计划

--- sqlite-oop/src/main/java/oop/sqlite/config/SqliteConfig.java
+++ sqlite-oop/src/main/java/oop/sqlite/config/SqliteConfig.java
@@ -1,5 +1,7 @@
 package oop.sqlite.config;
 
+import org.sqlite.SQLiteConfig;
+
 import java.io.IOException;
 import java.util.Properties;
 
@@ -7,10 +9,13 @@ import java.util.Properties;
  * sqlite的一些静态配置
  */
 public class SqliteConfig {
+
     /**
      * sqlite配置文件的配置信息
      */
     private static Properties properties = new Properties();
+
+    private static SQLiteConfig config = null;
 
     /**
      * 启动程序的时候读取properties配置文件信息，并永久缓存
@@ -20,8 +25,38 @@ public class SqliteConfig {
             //使用 properties 配置文件，默认在 config/sqlite.properties 目录下面，若该项目被引用，启动项目只需要在相同目录下相同配置文件覆盖即可生效
             properties.load(SqliteConfig.class.getClassLoader().getResourceAsStream("config/sqlite.properties"));
             //properties.loadFromXML(SqliteConfig.class.getClassLoader().getResourceAsStream("config/sqlite.xml"));
+            //创建Sqlite API 配置对象
+            config = new SQLiteConfig(properties);
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    /**
+     * 单例模式获取配置对象
+     * @return
+     */
+    public SQLiteConfig getConfig(){
+        if(null == config){
+            config = new SQLiteConfig(properties);
+        }
+        return config;
+    }
+
+    /**
+     * 动态加载新配置
+     * @param classPath
+     */
+    public void loadConfig(String classPath){
+        try {
+            //使用 properties 配置文件，默认在 config/sqlite.properties 目录下面，若该项目被引用，启动项目只需要在相同目录下相同配置文件覆盖即可生效
+            properties.load(SqliteConfig.class.getClassLoader().getResourceAsStream(classPath));
+            //properties.loadFromXML(SqliteConfig.class.getClassLoader().getResourceAsStream("config/sqlite.xml"));
+            //创建Sqlite API 配置对象
+            config = new SQLiteConfig(properties);
+        } catch (IOException e) {
+            e.printStackTrace();
+            properties = new Properties();
         }
     }
 

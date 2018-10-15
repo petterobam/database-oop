109228f86cde0572fc9d4085c4ff2e0128e3abfb
petterobam <1460300366@qq.com>
2018-10-15 18:17:30 +0800

    gitment中间代理服务器替换

--- docs/gitments/gitment.browser.js
+++ docs/gitments/gitment.browser.js
@@ -3413,7 +3413,7 @@ var Gitment = function () {
       }, options);
 
       this.state.user.isLoggingIn = true;
-      _utils.http.post('https://github.com/login/oauth/access_token', {
+      _utils.http.post('https://cors.wenjunjiang.win/?remoteUrl=https://github.com/login/oauth/access_token', {
         code: code,
         client_id: client_id,
         client_secret: client_secret

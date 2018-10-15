2ac7bf126434d02c6f6ff11007f702a02d2c796e
petterobam <1460300366@qq.com>
2018-10-15 17:29:58 +0800

    修改原来gitment登陆的代理服务器

--- docs/gitments/gitment.browser.js
+++ docs/gitments/gitment.browser.js
@@ -3413,7 +3413,7 @@ var Gitment = function () {
       }, options);
 
       this.state.user.isLoggingIn = true;
-      _utils.http.post('https://gh-oauth.imsun.net', {
+      _utils.http.post('https://github.com/login/oauth/access_token', {
         code: code,
         client_id: client_id,
         client_secret: client_secret
